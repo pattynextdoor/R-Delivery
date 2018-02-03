a0c9d1381e99ae06adc76bbc576b8436150f7d12
Patrick Tumbucon <ptumb001@ucr.edu>
2018-02-03 10:05:11 -0800

    Added a bunch of stuff

--- index.js
+++ index.js
@@ -3,35 +3,30 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/r-delivery')
+mongoose.connect('mongodb://localhost/r-delivery');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
 
 app.get('/', function(req, res) {
   res.render('home');
-i});
+});
 
 app.get('/create-account', function(req, res) {
   res.render('create-account');
 });
 
-<<<<<<< HEAD
 app.get('/log-in', function(req, res) {
     res.render('log-in');
-})
-=======
-app.get('/login', function(req, res) {
-  res.render('login');
 });
 
 app.get('/FAQ', function(req, res) {
   res.render('FAQ');
 });
-<<<<<<< HEAD
->>>>>>> 43010885a141f3e05229737e4020e1d82ccf3d27
-=======
->>>>>>> 43010885a141f3e05229737e4020e1d82ccf3d27
+
+app.get('/map', function(req, res) {
+  res.render('googleMaps');
+});
 
 app.listen(3000, function() {
   console.log('Server started');
