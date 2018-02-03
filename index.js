e9ef9d2b32f103d09f6f2fd69c0651fb33f35d2d
Douglas Tran <doug.tran.8@gmail.com>
2018-02-02 23:12:17 -0800

    Log In HTML

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ app.set('view engine','ejs');
 
 app.get('/', function(req, res) {
   res.render('home');
-});
+i});
 
 app.get('/create-account', function(req, res) {
   res.render('create-account');
+})
+
+app.get('/log-in', function(req, res) {
+    res.render('log-in');
 })
 
 app.listen(3000, function() {
