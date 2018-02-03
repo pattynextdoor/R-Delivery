9254b8c3174017f99be65e7b74361c9233233c61
Douglas Tran <doug.tran.8@gmail.com>
2018-02-02 23:48:06 -0800

    Create account and log in

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect('mongodb://localhost/r-delivery')
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
@@ -10,11 +14,21 @@ i});
 
 app.get('/create-account', function(req, res) {
   res.render('create-account');
-})
+});
 
+<<<<<<< HEAD
 app.get('/log-in', function(req, res) {
     res.render('log-in');
 })
+=======
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.get('/FAQ', function(req, res) {
+  res.render('FAQ');
+});
+>>>>>>> 43010885a141f3e05229737e4020e1d82ccf3d27
 
 app.listen(3000, function() {
   console.log('Server started');
