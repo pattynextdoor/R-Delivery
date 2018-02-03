fe46b24ca1ba741d9950b327fd92f0614f03e9c0
Patrick Tumbucon <ptumb001@ucr.edu>
2018-02-02 22:04:54 -0800

    Started account creation page

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine','ejs');
+
 app.get('/', function(req, res) {
-  res.send('this is the home page');
+  res.render('home');
 });
+
+app.get('/create-account', function(req, res) {
+  res.render('create-account');
+})
 
 app.listen(3000, function() {
   console.log('Server started');
